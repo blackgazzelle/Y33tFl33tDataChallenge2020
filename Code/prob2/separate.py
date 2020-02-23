95fd21d6f38a231a24082b928142ea8b87e35ab4
blackgazzelle <blackgazzelle@gmail.com>
2020-02-23 02:07:41 -0500

    DataSets sorted by Building type

--- Code/prob2/separate.py
+++ Code/prob2/separate.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df = pd.read_csv("../../DataSets/Exception_Cleaned_ARCHdata.csv")
 
-df[df["Primary_Property_Type_EPA_Calculated"] == "Mulitfamily Housing"].to_csv("MultiFamilyHousing.csv")
+for arg in pd.unique(df["Primary_Property_Type_EPA_Calculated"]):
+	df[df["Primary_Property_Type_EPA_Calculated"] == arg].to_csv(arg + '.csv')

517a819119d7f931554a192a048362273dca7b09
blackgazzelle <blackgazzelle@gmail.com>
2020-02-26 14:18:23 -0800

    New Separated building csvs

---
+++ Code/prob2/EnergyScore.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+directory = "../../DataSets/prob2sorted"
+scores=[]
+names=os.listdir(directory)
+i=0
+for name in names:
+    names[i] = names[i][:-4]
+    i+=1
+for files in os.scandir(directory):
+    df = pd.read_csv(files)
+    scores.append(df["Energy_Star_Score"].mean())
+plt.figure(figsize=(40, 3))  # width:20, height:3
+plt.bar(names, scores, align='edge', width=0.02)
+plt.show()
+
+

8e6e0716e211d00c772b81bf57b11813a9394a5a
blackgazzelle <blackgazzelle@gmail.com>
2020-02-27 13:04:06 -0800

    Kades comments suck

--- Code/prob2/EnergyScore.py
+++ Code/prob2/EnergyScore.py
@@ -13,7 +13,8 @@ for files in os.scandir(directory):
     df = pd.read_csv(files)
     scores.append(df["Energy_Star_Score"].mean())
 plt.figure(figsize=(40, 3))  # width:20, height:3
-plt.bar(names, scores, align='edge', width=0.02)
+plt.bar(names, scores, align='edge', width=.8)
+plt.xticks(rotation=45, ha='right')
 plt.show()
 
 
